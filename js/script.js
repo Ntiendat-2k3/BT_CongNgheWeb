42852e5a6cf33b660c2300243be238ebc1ed1849
NTiendat <nguyentiendatg2003@gmail.com>
2024-10-31 19:02:31 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const weekItems = document.querySelectorAll(".week-list li");
+  const weekContents = document.querySelectorAll(".week-content");
+
+  weekItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      weekItems.forEach((i) => i.classList.remove("active"));
+      item.classList.add("active");
+
+      weekContents.forEach((content) => (content.style.display = "none"));
+      const selectedWeek = document.getElementById(
+        item.getAttribute("data-week")
+      );
+      if (selectedWeek) {
+        selectedWeek.style.display = "block";
+      }
+
+      const activeDropdowns = document.querySelectorAll(
+        ".exercise-list li.active"
+      );
+      activeDropdowns.forEach((dropdown) =>
+        dropdown.classList.remove("active")
+      );
+    });
+  });
+
+  const exerciseTitles = document.querySelectorAll(".exercise-title");
+
+  exerciseTitles.forEach((title) => {
+    title.addEventListener("click", () => {
+      const parentLi = title.parentElement;
+
+      parentLi.classList.toggle("active");
+
+      const icon = title.querySelector("i");
+      if (parentLi.classList.contains("active")) {
+        icon.style.transform = "rotate(180deg)";
+      } else {
+        icon.style.transform = "rotate(0deg)";
+      }
+
+      exerciseTitles.forEach((otherTitle) => {
+        if (otherTitle !== title) {
+          otherTitle.parentElement.classList.remove("active");
+          otherTitle.querySelector("i").style.transform = "rotate(0deg)";
+        }
+      });
+    });
+  });
+});
