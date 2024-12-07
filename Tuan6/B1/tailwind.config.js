e1ce8c43bce6789a6fafa68a4d6832e227af5094
NTiendat <nguyentiendatg2003@gmail.com>
2024-12-07 23:39:36 +0700

    fix

--- Tuan6/B1/tailwind.config.js
+++ Tuan6/B1/tailwind.config.js
@@ -1,13 +1,7 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        mono: ['"Roboto Mono"', "monospace"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
